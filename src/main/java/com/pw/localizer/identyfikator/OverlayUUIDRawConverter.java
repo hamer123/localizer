dff05297e06555044e475261762ba9eb6d7ac2cb
wereckip <hamer123@vp.pl>
2016-09-06 10:46:56 +0200

    proba primefaces na 5.3

---
+++ src/main/java/com/pw/localizer/identyfikator/OverlayUUIDRawConverter.java
@@ -0,0 +1,28 @@
+package com.pw.localizer.identyfikator;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by wereckip on 06.09.2016.
+ */
+
+/**
+ * Kolejnosc i wzor uuid
+ * Overlay_
+ */
+class OverlayUUIDRawConverter {
+
+    private OverlayUUIDRawConverter(){}
+
+    public static String regex(OverlayUUIDRaw uuidRaw){
+        return null;
+    }
+
+    public static String uuid(OverlayUUIDRaw uuidRaw){
+        return null;
+    }
+
+    public static Pattern pattern(OverlayUUIDRaw uuidRaw){
+        return null;
+    }
+}
